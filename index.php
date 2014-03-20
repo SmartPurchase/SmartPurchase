85b1f898b8d1eca01fd3ce6a4980fe79e0300ec9
luucapelli <luh.capelli@hotmail.com>
2014-03-19 21:58:13 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+   <head>
+       <meta charset="utf-8"/>
+       <title>SmartPurchase - Sua compra inteligênte</title>
+   </head>
+   <body>
+    <?php
+    include('load_pages.php');
+    ?>
+   </body>
+</html>
